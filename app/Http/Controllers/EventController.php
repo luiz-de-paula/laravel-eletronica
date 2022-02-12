f5104bb71e6be580a1719f4d820b9c807b700c1c
luiz-de-paula <eletronica.do.lui@gmail.com>
2022-02-12 00:31:53 -0300

    Novas funcionalidades, busca e nome do criador do evento

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Event;
+use App\models\User;
 
 class EventController extends Controller
 {
@@ -27,7 +28,7 @@ class EventController extends Controller
 
     public function store(Request $request){
         $event = new Event;
-
+ 
         $event->title = $request->title;
         $event->date = $request->date;
         $event->city = $request->city ;
@@ -47,6 +48,8 @@ class EventController extends Controller
             $event->image = $imageName;
 
         }
+        $user = auth()->user();
+        $event->user_id = $user->id;
 
 
 
@@ -54,13 +57,12 @@ class EventController extends Controller
 
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
-
-
     }
     public function show($id){
         $event = Event::findOrFail($id);
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event'=>$event]);
+        return view('events.show', ['event'=>$event, 'eventOwner'=> $eventOwner]);
     }
 
 
